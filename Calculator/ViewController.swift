74b310069e613e0dff2f9133fa343e52304bb5cf
Sim Yong Seng <simyseng@gmail.com>
2019-09-03 19:46:38 +0700

    First commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sim Yong Seng on 31/8/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let x = 1
+        let y = 2
+        let z = x + y
+        print(z)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
